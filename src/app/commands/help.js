bcb8346af6d61a10e5186e67bf712a0e70ba4e06
ChronoAcceleration <l1e1x1c@gmail.com>
2025-08-21 19:54:21 -0700

    make commands, fix deprication err

--- src/app/commands/help.js
+++ src/app/commands/help.js
@@ -1,11 +1,11 @@
-import { ApplicationIntegrationType, InteractionContextType, MessageFlags, MessageFlagsBitField, SlashCommandBuilder } from 'discord.js';
+import { ApplicationIntegrationType, InteractionContextType, MessageFlags, SlashCommandBuilder } from 'discord.js';
 
 /**
  * @type {import('commandkit').CommandData}
  */
 export const command = new SlashCommandBuilder()
-  .setName("latency")
-  .setDescription("Check bot latency")
+  .setName("help")
+  .setDescription("How do I use you!??")
   .setContexts(InteractionContextType.BotDM, InteractionContextType.PrivateChannel)
   .setIntegrationTypes(ApplicationIntegrationType.UserInstall)
 
@@ -13,8 +13,6 @@ export const command = new SlashCommandBuilder()
  * @param {import('commandkit').MessageCommandContext} ctx
  */
 export const message = async (ctx) => {
-  const latency = (ctx.client.ws.ping ?? -1).toString();
-  const response = `latency: ${latency}ms`;
-
+  const response = `hey, i'm TwaatterMnotifier. all you gotta do is open my dm, paste in a twitter account and ill start monitoring them for you. Any changes will be seen and I will shoot you a message.`
   await ctx.message.reply({content: response, flags: MessageFlags.Ephemeral});
 };
